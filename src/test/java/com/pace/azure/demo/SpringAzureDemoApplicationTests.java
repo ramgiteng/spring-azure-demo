b58c0ba6992d4583f015be498f828c5507738b4a
ramgiteng <ramesh.raghunathan@gmail.com>
2023-10-22 17:50:53 -0400

    first commit

---
+++ src/test/java/com/pace/azure/demo/SpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pace.azure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
